210e21da17c5ef90245e2e8131d49be782aa7bae
Grayson Spencer <graysonspencer1212@gmail.com>
2021-10-03 21:40:40 -0600

    Main Menu Fixed
    Start button now functions.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //Instance of the menu manager that remains alive the entire life of the application.
+    public static MenuManager instance;
+
+    void Start()
+    {
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// Loads the first level.
+    /// </summary>
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
